15adc53561f72d1a9d9782bddbd5033b9f5f7563
Fredy Bautista <fredybautista@MacBook-Pro-de-Fredy.local>
2022-03-27 14:18:19 -0600

    topics app: add dev notes

---
+++ dev_notes.md
@@ -0,0 +1,10 @@
+# `Dev Notes``
+
+## `Future Improvements``
+
+1.- If the App is getting bigger and handle more date maybe we can consider use Context API or Redux \
+2.- Refactor the Api.js the function for create the connection to the Graphql can be placed into another method and that function can handle only the connection.
+3.- Move the queries for each endpoint into another file
+4.- Crate functions in especific for each endpoint
+5.- Handle exeptions
+6.- Consider use a library for UI such as reac-bootstrap, material design, ant desing, etc.
--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,39 +1,39 @@
 import { ApolloClient, createHttpLink, InMemoryCache, gql } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
-export async function getData(topicName) {
-const httpLink = createHttpLink({
-    uri: process.env.REACT_APP_GITHUB_URL,
-  });
+// method where the conection to the GraphQL API is created
+export const getData = async (topicName) => {
+    const httpLink = createHttpLink({
+        uri: process.env.REACT_APP_GITHUB_URL,
+    });
 
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-      }
-    }
-  });
+    const authLink = setContext((_, { headers }) => ({
+        headers: {
+            ...headers,
+            authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+            }
+        })
+    );
 
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-  });
+    const client = new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache()
+    });
 
-  const { data } = await client.query({
-    query: gql`
-      {
-        topic(name: "${topicName}") {
-            id
-            name
-            relatedTopics(first: 10){
-              name
-              id
-              stargazerCount
+    const { data } = await client.query({
+        query: gql`
+        {
+            topic(name: "${topicName}") {
+                id
+                name
+                relatedTopics(first: 10){
+                name
+                id
+                stargazerCount
+                }
             }
         }
-      }
-    `
-  });
-  return data.topic
+        `
+    });
+    return data.topic
 }
